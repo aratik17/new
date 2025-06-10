b59da675bccb950ee6f872f3a3bb1482d9cc49fb
aratik17 <aratikundargi17@gmail.com>
2025-06-10 13:55:05 +0530

    save the file

--- add.java
+++ add.java
@@ -1,9 +1,10 @@
-public class add{
-    int a=10;
-    int b=20;
-    
+public class add {
+    int a = 10;
+    int b = 20;
 
-    public static void main (String[] args){
-        System.out.println("a+b");
+    public static void main(String[] args) {
+        add obj = new add(); // Create an object to access non-static variables
+        int sum = obj.a + obj.b;
+        System.out.println("Sum of a and b is: " + sum);
     }
 }
